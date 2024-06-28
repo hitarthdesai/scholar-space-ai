cd5a643450e418d484dde2ed10a97f9f140f226e
Hitarth Desai <hitarthdesai306@gmail.com>
2024-06-28 12:21:08 -0400

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  useTabs: false,
+  endOfLine: "lf",
+  printWidth: 80,
+  proseWrap: "always"
+};
+
+export default config;
