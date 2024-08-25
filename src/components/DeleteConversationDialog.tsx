5c1c9bda86ff5641ff427b98f7f488b360b05c1e
Gitanshk <gitanshkothari@gmail.com>
2024-08-25 03:38:06 -0400

    added delete dialog

---
+++ src/components/DeleteConversationDialog.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DeleteIcon } from "lucide-react";
+import { type ReactNode, useState } from "react";
+
+type DeleteConversationDialogProps = {
+  conversationId: string;
+  trigger?: ReactNode;
+};
+
+function DefaultDeleteConversationTrigger() {
+  return (
+    <Button
+      variant="ghost"
+      className="flex aspect-video h-full min-w-52 max-w-52 items-center justify-center border-[1px] border-dashed"
+    >
+      <DeleteIcon className="h-16 w-16" />
+    </Button>
+  );
+}
+
+export function DeleteConversationDialog({
+  conversationId,
+  trigger = <DefaultDeleteConversationTrigger />,
+}: DeleteConversationDialogProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent className="max-w-72 sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Delete Conversation</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this conversation?
+          </DialogDescription>
+        </DialogHeader>
+        {/* <RenameConversationForm
+          conversationId={conversationId}
+          setIsOpen={setIsOpen}
+        /> */}
+        <DialogFooter>
+          <Button
+            type="submit"
+            className="bg-red-700 text-white hover:bg-red-800 focus:ring-2 focus:ring-red-500"
+          >
+            Delete
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
