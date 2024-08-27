98d57fee8ff36d03e74ed84c2ab48c7207ba7197
Hitarth Desai <83507760+hitarthdesai@users.noreply.github.com>
2024-08-27 17:32:02 +0530

    add no output component

--- src/components/assignment/OutputSection.tsx
+++ src/components/assignment/OutputSection.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { useCodeContext } from "@/contexts/CodeContext";
+import { InfoIcon } from "lucide-react";
 
 export function OutputSection() {
   const { output } = useCodeContext();
+  const isThereAnyOutput = output.length > 0;
 
   return (
     <>
       <div className="w-full text-center">Output</div>
-      <div className="w-full grow">
-        {output ?? "Run your code to get some output"}
+      <div className="h-full w-full grow text-muted">
+        {isThereAnyOutput ? (
+          <>{output}</>
+        ) : (
+          <div className="flex h-full w-full flex-col items-center justify-center">
+            <InfoIcon />
+            Run code to get some output
+          </div>
+        )}
       </div>
     </>
   );
