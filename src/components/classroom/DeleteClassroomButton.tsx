68241c3f909ed5462865845a2ffcd494ae791d95
Gitanshk <gitanshkothari@gmail.com>
2024-08-29 03:25:11 -0400

    added delete classroom dialog

--- src/components/classroom/DeleteClassroomButton.tsx
+++ src/components/classroom/DeleteClassroomButton.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { useAction } from "next-safe-action/hooks";
-import { toast } from "../ui/use-toast";
-import { deleteClassroom } from "@/actions/deleteClassroom";
-import { EnumDeleteClassroomResult } from "@/schemas/classroomSchema";
-import { toastDescriptionDeleteClassroom } from "@/utils/constants/toast";
 import { Button } from "../ui/button";
 import { TrashIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { DeleteClassroomDialog } from "./DeleteClassroomDialog";
 
 type DeleteClassroomButtonProps = {
   classroomId: string;
@@ -16,29 +11,14 @@ type DeleteClassroomButtonProps = {
 export function DeleteClassroomButton({
   classroomId,
 }: DeleteClassroomButtonProps) {
-  const router = useRouter();
-  const { executeAsync } = useAction(deleteClassroom, {
-    onSuccess({ data }) {
-      if (!data?.type) return;
-
-      const isErroneous =
-        data.type !== EnumDeleteClassroomResult.ClassroomDeleted;
-
-      toast({
-        title: isErroneous
-          ? "Error in deleting classroom"
-          : "Classroom deleted successfully",
-        description: toastDescriptionDeleteClassroom[data.type],
-        variant: isErroneous ? "destructive" : "default",
-      });
-
-      router.refresh();
-    },
-  });
-
   return (
-    <Button onClick={() => executeAsync({ classroomId })} variant="destructive">
-      <TrashIcon />
-    </Button>
+    <DeleteClassroomDialog
+      classroomId={classroomId}
+      trigger={
+        <Button variant="destructive">
+          <TrashIcon />
+        </Button>
+      }
+    />
   );
 }
---
+++ src/components/classroom/DeleteClassroomDialog.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TrashIcon } from "lucide-react";
+import { type ReactNode, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAction } from "next-safe-action/hooks";
+import { deleteClassroom } from "@/actions/deleteClassroom";
+import { EnumDeleteClassroomResult } from "@/schemas/classroomSchema";
+import { toastDescriptionDeleteClassroom } from "@/utils/constants/toast";
+import { toast } from "../ui/use-toast";
+
+type DeleteClassroomDialogProps = {
+  classroomId: string;
+  trigger?: ReactNode;
+};
+
+function DefaultDeleteClassroomTrigger() {
+  return (
+    <Button
+      variant="ghost"
+      className="flex aspect-video h-full min-w-52 max-w-52 items-center justify-center border-[1px] border-dashed"
+    >
+      <TrashIcon className="h-16 w-16" />
+    </Button>
+  );
+}
+
+export function DeleteClassroomDialog({
+  classroomId,
+  trigger = <DefaultDeleteClassroomTrigger />,
+}: DeleteClassroomDialogProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
+
+  const { executeAsync } = useAction(deleteClassroom, {
+    onSuccess({ data }) {
+      if (!data?.type) return;
+
+      const isErroneous =
+        data.type !== EnumDeleteClassroomResult.ClassroomDeleted;
+
+      toast({
+        title: isErroneous
+          ? "Error in deleting classroom"
+          : "Classroom deleted successfully",
+        description: toastDescriptionDeleteClassroom[data.type],
+        variant: isErroneous ? "destructive" : "default",
+      });
+      if (!isErroneous) {
+        setIsOpen(false);
+        router.refresh();
+      }
+    },
+  });
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent className="max-w-72 sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Delete Classroom</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this classroom?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            type="submit"
+            className="bg-red-700 text-white hover:bg-red-800 focus:ring-2 focus:ring-red-500"
+            onClick={() => executeAsync({ classroomId })}
+          >
+            Delete
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
