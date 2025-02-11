2b4f55b22b40bc48ac29f93b920bda74571848e8
Hitarth Desai <83507760+hitarthdesai@users.noreply.github.com>
2025-02-11 16:15:30 -0500

    first draft

--- src/components/question/add/AddCodeQuestionFormFields.tsx
+++ src/components/question/add/AddCodeQuestionFormFields.tsx
@@ -8,7 +8,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { type AddQuestionForm } from "@/schemas/questionSchema";
 import { type UseFormReturn } from "react-hook-form";
@@ -19,51 +18,21 @@ type AddCodeQuestionFormProps = {
 
 export function AddCodeQuestionFormFields({ form }: AddCodeQuestionFormProps) {
   return (
-    <>
-      <FormField
-        control={form.control}
-        name="name"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Name</FormLabel>
-            <FormControl>
-              <Input required {...field} />
-            </FormControl>
-            <FormDescription>Name of the question</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="question"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Question</FormLabel>
-            <FormControl>
-              <Textarea required {...field} />
-            </FormControl>
-            <FormDescription>The question text</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="starterCode"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Starter code</FormLabel>
-            <FormControl>
-              <Textarea required {...field} />
-            </FormControl>
-            <FormDescription>
-              Stubbed code to assist the user in solving questions
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </>
+    <FormField
+      control={form.control}
+      name="starterCode"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Starter code</FormLabel>
+          <FormControl>
+            <Textarea required {...field} />
+          </FormControl>
+          <FormDescription>
+            Stubbed code to assist the user in solving questions
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
   );
 }
