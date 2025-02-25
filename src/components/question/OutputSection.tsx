2cb1498a3e1f0385c0c5b5fa88ca61f5053c527e
LalithElangovan <lalith.elangovan@gmail.com>
2025-02-25 13:46:38 -0500

    Added code review changes

--- src/components/question/OutputSection.tsx
+++ src/components/question/OutputSection.tsx
@@ -2,27 +2,24 @@
 
 import { useCodeContext } from "@/contexts/CodeContext";
 import { InfoIcon } from "lucide-react";
+import cn from "classnames";
 
 export function OutputSection() {
-  const { output } = useCodeContext();
+  const { output, status } = useCodeContext();
   const isThereAnyOutput = output.length > 0;
-  const isError = output.toLowerCase().includes("traceback");
-  const isWarning = output.toLowerCase().includes("warning");
+
+  const colorClass = cn({
+    "text-red-500": status === "1",
+    "text-white": status === "0",
+    "text-yellow-500": status !== "0" && status !== "1",
+  });
 
   return (
     <>
       <div className="w-full text-center">Output</div>
       <div className="h-full w-full grow text-muted">
         {isThereAnyOutput ? (
-          <div
-            style={{
-              color: isError ? "red" : isWarning ? "lightyellow" : "white",
-              fontFamily: "monospace",
-              fontSize: "16px",
-            }}
-          >
-            {output}
-          </div>
+          <div className={cn(colorClass, "font-mono text-lg")}>{output}</div>
         ) : (
           <div className="flex h-full w-full flex-col items-center justify-center">
             <InfoIcon />
