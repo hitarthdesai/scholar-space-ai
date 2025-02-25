ad08956c1420a18cd58c263b2f62d165692a6711
LalithElangovan <lalith.elangovan@gmail.com>
2025-02-25 00:17:06 -0500

    Really simple change to change the font colour

--- src/components/question/OutputSection.tsx
+++ src/components/question/OutputSection.tsx
@@ -6,13 +6,23 @@ import { InfoIcon } from "lucide-react";
 export function OutputSection() {
   const { output } = useCodeContext();
   const isThereAnyOutput = output.length > 0;
+  const isError = output.toLowerCase().includes("traceback");
+  const isWarning = output.toLowerCase().includes("warning");
 
   return (
     <>
       <div className="w-full text-center">Output</div>
       <div className="h-full w-full grow text-muted">
         {isThereAnyOutput ? (
-          <>{output}</>
+          <div
+            style={{
+              color: isError ? "red" : isWarning ? "lightyellow" : "white",
+              fontFamily: "monospace",
+              fontSize: "16px",
+            }}
+          >
+            {output}
+          </div>
         ) : (
           <div className="flex h-full w-full flex-col items-center justify-center">
             <InfoIcon />
