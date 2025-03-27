6c14c40bcbd9557bafb7c22008a217aa702ce613
Gitanshk <gitanshkothari@gmail.com>
2025-03-27 19:27:08 -0400

    used shadcn button with icon

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
@@ -13,15 +14,17 @@ export function ThemeSwitcher() {
   if (!mounted) return null;
 
   return (
-    <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    <Button
+      variant="ghost"
+      size="icon"
       className="flex h-8 w-8 items-center justify-center rounded-lg border bg-background transition hover:bg-accent"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {theme === "dark" ? (
         <SunIcon className="h-4 w-4" />
       ) : (
         <MoonIcon className="h-4 w-4" />
       )}
-    </button>
+    </Button>
   );
 }
